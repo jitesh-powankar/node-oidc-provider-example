ce1b97715f1c1ca50649b40042e564a7c0d3f9c0
Filip Skokan <panva.ip@gmail.com>
2019-05-07 17:05:43 +0200

    v6.0.0 upgrade

--- 02-oidc-adapter/index.js
+++ 02-oidc-adapter/index.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 const Provider = require('oidc-provider');
 
-// since dyno metadata is no longer available, we infer the app name from heroku remote we set
+// since dyno metadata is no longer available, you infer the app name from heroku remote you set
 // manually. This is not specific to oidc-provider, just an easy way of getting up and running
 if (!process.env.HEROKU_APP_NAME && process.env.X_HEROKU_REMOTE) {
   process.env.X_HEROKU_REMOTE.match(/\.com\/(.+)\.git/);
@@ -16,29 +16,14 @@ assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addo
 assert.equal(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
 assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`');
 
+const jwks = require('./jwks.json');
+
 // require the redis adapter factory/class
 const RedisAdapter = require('./redis_adapter');
 
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
-  formats: {
-    AccessToken: 'jwt',
-  },
-  features: {
-    claimsParameter: true,
-    discovery: true,
-    encryption: true,
-    introspection: true,
-    registration: true,
-    request: true,
-    revocation: true,
-    sessionManagement: true,
-  },
-});
-
-const keystore = require('./keystore.json');
-
-oidc.initialize({
-  keystore,
+  // configure Provider to use the adapter
+  adapter: RedisAdapter,
   clients: [
     // reconfigured the foo client for the purpose of showing the adapter working
     {
@@ -49,10 +34,17 @@ oidc.initialize({
       token_endpoint_auth_method: 'none',
     },
   ],
-  // configure Provider to use the adapter
-  adapter: RedisAdapter,
-}).then(() => {
-  oidc.proxy = true;
-  oidc.keys = process.env.SECURE_KEY.split(',');
-  oidc.listen(process.env.PORT);
+  jwks,
+  formats: {
+    AccessToken: 'jwt',
+  },
+  features: {
+    encryption: { enabled: true },
+    introspection: { enabled: true },
+    revocation: { enabled: true },
+  },
 });
+
+oidc.proxy = true;
+oidc.keys = process.env.SECURE_KEY.split(',');
+oidc.listen(process.env.PORT);
